559c309e5cc9aee7f633f5a47aa5146b7eaff248
Kevin Li <kevinli020508@gmail.com>
2023-03-01 13:37:22 -0500

    Can search for entries

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -6,14 +6,20 @@ namespace KoDict.Pages;
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
-    public Dict dict = new Dict();
+    private Dict Dict = new Dict();
+    [BindProperty(SupportsGet = true)]
+    public string? SearchString { get; set; }
+    public Dict.Entry? Entry;
 
     public IndexModel(ILogger<IndexModel> logger)
     {
         _logger = logger;
     }
-    public void OnGet()
+    public async Task OnGetAsync()
     {
-        
+        if (!string.IsNullOrEmpty(SearchString))
+        {
+            this.Entry = this.Dict.entries.Where(s => s.word == SearchString).FirstOrDefault();
+        }
     }
 }
